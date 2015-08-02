1bb86886269c1dcf1c79604fafc833e3eaffe55c
Samarth Asthana <samarth.asthana@cerner.com>
2015-08-02 18:17:31 -0500

    Fixed formatting again

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -13,22 +13,10 @@ mainPageControllers.controller('SectionCtrl', ['$scope', '$location','$http',
           $scope.projects = data_projects.content;
         });
       }
-      // if($location.path() === '/academics'){
-      //   var content_list = data.content;
-      //   for( var indx = 0; indx < content_list.length; indx++){
-      //     var list_item = document.createElement("li");
-      //     list_item.id = "list-item-"+indx;
-      //     list_item.innerHTML = "<p id = ins-"+ indx +">"+content_list[indx].insitute+"</p>
-      //                           <br><p></p>";
-      //     document.getElementById('section-body').appendChild(list_item);
-      //   }
-      // }
     });
  }]);
 
  //private methods
-
-
 
 // mainPageControllers.controller('SectionCtrl', ['$scope', '$routeParams','$http',
 //   function($scope, $routeParams, $http) {
